e6c2c59d8b649baf0e981c19ac96d83d555f1b14
Markus Karjalainen <info@markuskarjalainen.com>
2014-05-18 00:01:41 +0300

    For testing.

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ function previous(idx) {
 function displayObservation(idx) {
 	
 	var uvIndex = observatories["data"][idx].index;
-	//uvIndex = Math.round((((uvIndex * 1) + Math.random() * 12) + 0.3) * 10) / 10;
+	uvIndex = Math.round((((uvIndex * 1) + Math.random() * 12) + 0.3) * 10) / 10;
 	//uvIndex = 12.5;
 	document.getElementById("measurement").innerHTML = uvIndex;
 	document.getElementById("location").innerHTML = 
