751a38d634360cf73baa1c07dd5fe51cf6e51a25
Markus Karjalainen <info@markuskarjalainen.com>
2014-05-19 23:59:10 +0300

    Modified measurement logic.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var classification = {
 
 var observatories = null;
 var index = -1;
-var start = new Date(new Date().getTime() - (3 * 60 * 60 * 1000));
+var start = new Date(new Date().getTime() - (1 * 60 * 60 * 1000));
 var end = new Date();
 
 function init() {
@@ -122,9 +122,7 @@ function displayObservation(idx) {
 		observatories["data"][idx].name;
 	document.getElementById("origin").innerHTML =
 		getDate(new Date()) + " " + getMessage("origin") + "<br />" + 
-		getTime(start) + " - " + getTime(end) + " " + getMessage("averageof") + " " + 
-		observatories["data"][idx].observations + " " + 
-		getMessage("observations") + ".";
+		getMessage("observationtime") + ": " + getTime(start) + " - " + getTime(end);
 
 	var baseline = ((window.innerWidth - 320) / 2);
 	
